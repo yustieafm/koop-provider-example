9ffc2db0e9f3f04c38ac5d51cb22360370cf2956
yustieafm <59076640+yustieafm@users.noreply.github.com>
2020-04-27 13:34:56 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,9 +9,15 @@
 // Define the provider path
 // /:name/:hosts?/:disableIdParam?/FeatureServer/:layer/:method
 // e.g. /example/FeatureServer/0/query
+const express = require('express')
+const app = express()
+const fs = require('fs')
+const https = require('https')
+const Koop = require('koop')
+const koop = new Koop()
 const provider = {
   type: 'provider',
-  name: 'sebaranjabar',
+  name: 'example',
   hosts: false, // if true, also adds disableIdParam
   disableIdParam: true, // if true, adds to path and req.params
   Controller: require('./controller'),
@@ -19,5 +25,10 @@ const provider = {
   routes: require('./routes'),
   version: require('../package.json').version
 }
+koop.register(provider)
+app.use('/koop', koop.server)
+https.createServer({
+  key: fs.readFileSync("D:/koop-provider-example/server.key"),
+  cert: fs.readFileSync("D:/koop-provider-example/server.cert")}, app).listen(8443);
 
 module.exports = provider
